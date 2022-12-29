b56203a2062356af9f9c3845daedfb48d7d98c9f
paschal533 <okwuosahpaschal@gmail.com>
2022-12-29 23:47:55 +0530

    first commit

--- utils/client.ts
+++ utils/client.ts
@@ -4,6 +4,6 @@ export const client = sanityClient({
   projectId: 'tp4ln3j8',
   dataset: 'production',
   apiVersion: '2022-03-10',
-  useCdn: false,
+  //useCdn: false,
   token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
 });
