c46d3338fadfa5ae45d3660a6d8c668f307a9ca9
paschal533 <okwuosahpaschal@gmail.com>
2023-01-06 23:07:25 +0530

    components update

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Token {
-    string public name;
+    //string public name;
     string public symbol;
     uint256 public decimals = 18;
     uint256 public totalSupply;
